3785d82a618c177fa897126ff40212382ce2a60c
Jerel Nielsen <jerel.nielsen@gmail.com>
2020-05-01 13:10:30 -0600

    mapping raw data by duplicating class

--- src/memory_manipulation/main.cpp
+++ src/memory_manipulation/main.cpp
@@ -4,47 +4,83 @@
 
 using namespace std;
 
-class Vector
+class Quaternion
 {
 public:
-    double* ptr = NULL;
-    bool* internal_memory = NULL;
-
-    Vector() {}
-
-    ~Vector()
+    enum
     {
-        if (internal_memory) delete[] ptr;
+        _w,
+        _x,
+        _y,
+        _z,
+        _size
+    };
+
+    double buf[_size];
+
+    Quaternion()
+    {
+        buf[_w] = 1.0;
+        buf[_x] = 0.0;
+        buf[_y] = 0.0;
+        buf[_z] = 0.0;
     }
 
-    Vector(double* const _ptr)
+    Quaternion(const double& w, const double& x, const double& y, const double& z)
     {
-        ptr = _ptr;
-    }
-
-    Vector(double w, double x, double y, double z)
-    {
-        internal_memory = new bool;
-        ptr = new double[4];
-        ptr[0] = w;
-        ptr[1] = x;
-        ptr[2] = y;
-        ptr[3] = z;
+        buf[_w] = w;
+        buf[_x] = x;
+        buf[_y] = y;
+        buf[_z] = z;
     }
 
     void normalize()
     {
         double mag = sqrt(w()*w() + x()*x() + y()*y() + z()*z());
-        ptr[0] /= mag;
-        ptr[1] /= mag;
-        ptr[2] /= mag;
-        ptr[3] /= mag;
+        buf[_w] /= mag;
+        buf[_x] /= mag;
+        buf[_y] /= mag;
+        buf[_z] /= mag;
     }
 
-    double& w() { return ptr[0]; }
-    double& x() { return ptr[1]; }
-    double& y() { return ptr[2]; }
-    double& z() { return ptr[3]; }
+    const double& w() const { return buf[0]; }
+    const double& x() const { return buf[1]; }
+    const double& y() const { return buf[2]; }
+    const double& z() const { return buf[3]; }
+};
+
+class QuaternionMap
+{
+public:
+    enum
+    {
+        _w,
+        _x,
+        _y,
+        _z,
+        _size
+    };
+
+    double * buf = NULL;
+
+    QuaternionMap(double * ptr)
+    { 
+        buf = ptr;
+    }
+
+    void normalize()
+    {
+        double mag = sqrt(w()*w() + x()*x() + y()*y() + z()*z());
+        buf[_w] /= mag;
+        buf[_x] /= mag;
+        buf[_y] /= mag;
+        buf[_z] /= mag;
+    }
+
+    const double& w() const { return buf[0]; }
+    const double& x() const { return buf[1]; }
+    const double& y() const { return buf[2]; }
+    const double& z() const { return buf[3]; }
 };
 
 int main()
@@ -55,14 +91,14 @@ int main()
         cout << a[i] << " ";
     cout << endl;
 
-    Vector b(a[0], a[1], a[2], a[3]);
+    Quaternion b(a[0], a[1], a[2], a[3]);
     b.normalize();
     cout << "a_first = ";
     for (int i = 0; i < 4; ++i)
         cout << a[i] << " ";
     cout << endl;
 
-    Vector c(a);
+    QuaternionMap c(a);
     c.normalize();
     cout << "a_second = ";
     for (int i = 0; i < 4; ++i)
