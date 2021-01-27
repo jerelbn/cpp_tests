776476d22549e212991569a3ce5d790efeec29a4
Jerel Nielsen <Jerel.Nielsen@flir.com>
2021-01-27 16:08:10 -0700

    cleanup rel pose test

--- src/relative_camera_pose/rel_pose.cpp
+++ src/relative_camera_pose/rel_pose.cpp
@@ -13,8 +13,7 @@ vector<Point> predictPointLocs(const vector<Point>& pts, const Matrix3f& K, cons
                                const common::Quaternionf& q, const Vector3f& t)
 {
     vector<Point> new_pts = pts;
-    for (int i = 0; i < new_pts.size(); ++i)
-    {
+    for (int i = 0; i < new_pts.size(); ++i) {
         Vector3f pt_h(pts[i].x, pts[i].y, 1.0);
         Vector3f pt_c = pts[i].depth * (K_inv * pt_h).normalized();
         Vector3f new_pt_c = q.rotp(pt_c) + t;
@@ -29,12 +28,29 @@ vector<Point> predictPointLocs(const vector<Point>& pts, const Matrix3f& K, cons
 }
 
 
-template<int N>
-Matrix<float,N,1> computeRadioErrorAll(const vector<Point> &pts2_hat, const vector<Point> &pts2)
+vector<Point> predictPointLocsMeanDepth(const vector<Point>& pts, const Matrix3f& K, const Matrix3f& K_inv,
+                                        const common::Quaternionf& q, const Vector3f& t, float depth)
 {
+    vector<Point> new_pts = pts;
+    for (int i = 0; i < new_pts.size(); ++i) {
+        Vector3f pt_h(pts[i].x, pts[i].y, 1.0);
+        Vector3f pt_c = depth * (K_inv * pt_h).normalized();
+        Vector3f new_pt_c = q.rotp(pt_c) + t;
+        Vector3f new_pt_h = K*new_pt_c;
+        new_pt_h /= new_pt_h(2);
+        new_pts[i].x = new_pt_h(0);
+        new_pts[i].y = new_pt_h(1);
+        new_pts[i].depth = new_pt_c.norm();
+    }
+
+    return new_pts;
+}
+
+
+template<int N>
+Matrix<float,N,1> computeRadioErrorAll(const vector<Point> &pts2_hat, const vector<Point> &pts2) {
     Matrix<float,N,1> err;
-    for (int i = 0; i < N; ++i)
-    {
+    for (int i = 0; i < N; ++i) {
         float dx = pts2[i].x - pts2_hat[i].x;
         float dy = pts2[i].y - pts2_hat[i].y;
         err(i) = 0.5 * (dx * dx + dy * dy);
@@ -44,11 +60,57 @@ Matrix<float,N,1> computeRadioErrorAll(const vector<Point> &pts2_hat, const vect
 }
 
 
+template<int N>
+int solveQT(int iters, float eps, const Matrix3f &K, const Matrix3f &K_inv, 
+            const vector<Point> &pts1, const vector<Point> &pts2, common::Quaternionf &q_hat, Vector3f &t_hat) {
+    int ii;
+    const Matrix3f I = eps*Matrix3f::Identity();
+    for (ii = 0; ii < iters; ++ii) {
+        // Compute Jacobian of error w.r.t. rotation and translation
+        Matrix<float,N,6> J;
+        for (int i = 0; i < 3; ++i)
+        {
+            common::Quaternionf qp = q_hat +  I.col(i);
+            common::Quaternionf qm = q_hat + -I.col(i);
+            vector<Point> pts2p = predictPointLocs(pts1, K, K_inv, qp, t_hat);
+            vector<Point> pts2m = predictPointLocs(pts1, K, K_inv, qm, t_hat);
+            Matrix<float,N,1> errp = computeRadioErrorAll<N>(pts2p , pts2);
+            Matrix<float,N,1> errm = computeRadioErrorAll<N>(pts2m , pts2);
+            J.col(i) = (errp - errm)/(2.0*eps);
+        }
+        for (int i = 0; i < 3; ++i)
+        {
+            Vector3f tp = t_hat +  I.col(i);
+            Vector3f tm = t_hat + -I.col(i);
+            vector<Point> pts2p = predictPointLocs(pts1, K, K_inv, q_hat, tp);
+            vector<Point> pts2m = predictPointLocs(pts1, K, K_inv, q_hat, tm);
+            Matrix<float,N,1> errp = computeRadioErrorAll<N>(pts2p , pts2);
+            Matrix<float,N,1> errm = computeRadioErrorAll<N>(pts2m , pts2);
+            J.col(i+3) = (errp - errm)/(2.0*eps);
+        }
+
+        // Computer current error
+        vector<Point> pts2_hat = predictPointLocs(pts1, K, K_inv, q_hat, t_hat);
+        Matrix<float,N,1> err = computeRadioErrorAll<N>(pts2_hat, pts2);
+
+        // Update R/t estimates
+        Matrix<float,6,1> delta = J.householderQr().solve(err);
+        q_hat += -delta.segment<3>(0);
+        t_hat += -delta.segment<3>(3);
+
+        if (delta.norm() < 1e-6)
+            break;
+    }
+
+    return ii;
+}
+
+
 int main(int argc, char* argv[])
 {
     // General parameters
     const int Np = 10; // number of points
-    const int gn_iters = 100; // maximum number of Gauss Newton iterations
+    const int gn_iters = 1000; // maximum number of Gauss Newton iterations
     const float gn_eps = 1e-4; // Nudge for computing derivatives
 
     // Random parameters
@@ -87,9 +149,6 @@ int main(int argc, char* argv[])
     D(3) = 0;
     D(4) = 0;
 
-    // Constants
-    const Matrix3f I = gn_eps*Matrix3f::Identity();
-
     // Camera poses in camera coordinates (right-down-forward)
     // - camera 1 is the origin at null attitude
     Vector3f p1 = Vector3f::Zero();
@@ -117,8 +176,7 @@ int main(int argc, char* argv[])
     for (int i = 0; i < Np; ++i) {
         pts1[i].x = fx*lms1(0,i)/lms1(2,i) + cx;
         pts1[i].y = fy*lms1(1,i)/lms1(2,i) + cy;
-        // pts1[i].depth = lms1.col(i).norm();
-        pts1[i].depth = 10.0;
+        pts1[i].depth = lms1.col(i).norm();
         pts2[i].x = fx*lms2(0,i)/lms2(2,i) + cx;
         pts2[i].y = fy*lms2(1,i)/lms2(2,i) + cy;
         pts2[i].depth = lms2.col(i).norm();
@@ -127,46 +185,10 @@ int main(int argc, char* argv[])
     // Solve for relative camera rotation and translation via Gauss Newton optimization
     common::Quaternionf q_hat;
     Vector3f t_hat = Vector3f::Zero();
-    int ii;
-    for (ii = 0; ii < gn_iters; ++ii)
-    {
-        // Compute Jacobian of error w.r.t. rotation and translation
-        Matrix<float,Np,6> J;
-        for (int i = 0; i < 3; ++i)
-        {
-            common::Quaternionf qp = q_hat +  I.col(i);
-            common::Quaternionf qm = q_hat + -I.col(i);
-            vector<Point> pts2p = predictPointLocs(pts1, K, K_inv, qp, t_hat);
-            vector<Point> pts2m = predictPointLocs(pts1, K, K_inv, qm, t_hat);
-            Matrix<float,Np,1> errp = computeRadioErrorAll<Np>(pts2p , pts2);
-            Matrix<float,Np,1> errm = computeRadioErrorAll<Np>(pts2m , pts2);
-            J.col(i) = (errp - errm)/(2.0*gn_eps);
-        }
-        for (int i = 0; i < 3; ++i)
-        {
-            Vector3f tp = t_hat +  I.col(i);
-            Vector3f tm = t_hat + -I.col(i);
-            vector<Point> pts2p = predictPointLocs(pts1, K, K_inv, q_hat, tp);
-            vector<Point> pts2m = predictPointLocs(pts1, K, K_inv, q_hat, tm);
-            Matrix<float,Np,1> errp = computeRadioErrorAll<Np>(pts2p , pts2);
-            Matrix<float,Np,1> errm = computeRadioErrorAll<Np>(pts2m , pts2);
-            J.col(i+3) = (errp - errm)/(2.0*gn_eps);
-        }
-
-        // Computer current error
-        vector<Point> pts2_hat = predictPointLocs(pts1, K, K_inv, q_hat, t_hat);
-        Matrix<float,Np,1> err = computeRadioErrorAll<Np>(pts2_hat, pts2);
-
-        // Update R/t estimates
-        Matrix<float,6,1> delta = J.householderQr().solve(err);
-        q_hat += -delta.segment<3>(0);
-        t_hat += -delta.segment<3>(3);
-
-        if (delta.norm() < 1e-6)
-            break;
-    }
+    int num_iters = solveQT<Np>(gn_iters, gn_eps, K, K_inv, pts1, pts2, q_hat, t_hat);
     
-    cout << "num iters: " << ii << endl;
+    // Print results
+    cout << "num iters: " << num_iters << endl;
     cout << "q = \n" << q.toEigen() << endl;
     cout << "q_hat = \n" << q_hat.toEigen() << endl;
     cout << "t = \n" << t << endl;
